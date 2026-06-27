cbad419d0488a21ac54ca6047a329a0dfd5ac411
Mod Maintainer <mod@bilitickerbuy.local>
2026-06-27 22:59:20 +0800

    chore: remove accidental test file commits

--- test_doh.py
+++
@@ -1,9 +0,0 @@
-import json, urllib.request
-
-req = urllib.request.Request('https://dns.google/resolve?name=sg.nexip.cc&type=A')
-resp = urllib.request.urlopen(req, timeout=5)
-data = json.loads(resp.read().decode())
-for a in data.get('Answer', []):
-    atype = a['type']
-    adata = a['data']
-    print(f'type={atype} data={adata}')
